8f70ba833e706ffb54630c7d5f437b02758ae892
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-09-08 10:16:28 +0200

    Rewrite code to be array based and add printBoard method

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,15 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const printBoard = board => {
+  console.log("Current Board:");
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
