476d0f704c2854ecb894b614307c17f4589310bd
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-09-08 10:16:29 +0200

    Dynamically generate boards and generate bombs randomly

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,39 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowNum = 1; rowNum <= numberOfRows; rowNum++) {
+    let row = [];
+    for (let columnNum = 1; columnNum <= numberOfColumns; columnNum++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowNum = 1; rowNum <= numberOfRows; rowNum++) {
+    let row = [];
+    for (let columnNum = 1; columnNum <= numberOfColumns; columnNum++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced <= numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    // Will potentially place on top of already existing bomb
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+  return board;
+}
 const printBoard = board => {
-  console.log("Current Board:");
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
