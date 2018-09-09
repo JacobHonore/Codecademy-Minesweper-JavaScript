8e0d66c11bb9bd496360651fae778621a3716682
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-09-09 10:10:26 +0200

    Fix so it is not possible to place bomb on existing bomb and add function for getting number of neighbor bombs

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -22,11 +22,31 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   while (numberOfBombsPlaced <= numberOfBombs) {
     let randomRowIndex = Math.floor(Math.random() * numberOfRows);
     let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    // Will potentially place on top of already existing bomb
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++;
+    if (board[randomRowIndex][randomColumnIndex] != 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
   }
   return board;
+}
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset;
+    const neighborColumnIndex = columnIndex + offset;
+    if (
+      neighborRowIndex >= 0 && neighborRowIndex < numberOfRows &&
+      neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns
+    ) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] == 'B') {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
 }
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
