d085ded2db0679c1e4dce9fb0e564c2720c8461e
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-09-13 21:39:42 +0200

    Implement Game class and make one move

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,23 @@
+class Game {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] == 'B'){
+      console.log("Game over!");
+      this._board.print();
+    }
+    else if (!this._board.hasSafeTiles()) {
+      console.log("Congratulations, you won the awesome Minesweeper game!");
+      this._board.print();
+    }
+    else {
+      console.log("Current Board:");
+      this._board.print();
+    }
+  }
+}
 class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
@@ -81,12 +101,5 @@ class Board {
     return board;
   }
 }
-const playerBoard = generatePlayerBoard(3, 4);
-const bombBoard = generateBombBoard(3, 4, 5);
-console.log('Player Board:');
-printBoard(playerBoard);
-console.log('Bomb Board:');
-printBoard(bombBoard);
-flipTile(playerBoard, bombBoard, 0, 0);
-console.log('Updated Player Board:');
-printBoard(playerBoard);
+const g = new Game(3, 3, 3);
+g.playMove(0, 0);
